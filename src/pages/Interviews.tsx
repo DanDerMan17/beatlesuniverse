1985575b0423b08cf1e2ad4896a5aed924f3d0e3
fekdac22 <fekdac22@htl-kaindorf.at>
2026-02-22 18:42:51 +0100

    story til 1963 written

--- src/pages/Interviews.tsx
+++ src/pages/Interviews.tsx
@@ -2,6 +2,24 @@ export default function Interviews() {
     return (
         <div className="page">
             <h2>Interesting Interviews & Resources</h2>
+
+            <article>
+                <section className="resource" id="george-interview">
+                    <h3>George Harrison Interview</h3>
+                    <p>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube.com/embed/7TeqyovfJzE?si=9Zqoo5OdTLk5iuhL"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+                    </p>
+                    <p>
+                        adkasjdka
+                    </p>
+                </section>
+            </article>
+
             <article>
                 <section className="resource">
                     <h3>Resource Title 1</h3>
--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -66,6 +66,13 @@ export default function Story() {
                     </ul>
                 </p>
 
+                <p style={{ fontWeight: "bold"}}>1956</p>
+                <p>
+                    <ul>
+                        <li>October 31: Paul's mother, Mary, dies of breast cancer.</li>
+                    </ul>
+                </p>
+
                 <p style={{ fontWeight: "bold"}}>1957</p>
                 <p>
                     Paul and George begin playing guitars together in Harrison's front room. John starts a skiffle group, who eventually become the Quarryman named after the school that most of its members attended.
@@ -80,16 +87,243 @@ export default function Story() {
                 <p>
                     <ul>
                         <li>January 24: Paul plays in the Cavern, and a few weeks later, George joins.</li>
-                        <li>July 15: Lennon's mother, Julia Lennon, is run over and killed by a car driven by an off-duty, drunk policeman.</li>
+                        <li>
+                            July 9: The Quarrymen pay 17 pounds and 6 pence to record two songs at a local recording
+                            studio in Liverpool, namely "That'll Be the Day" by Buddy Holy and "In Spite of All the
+                            Danger" by Harrison and McCartney. <br></br>
+
+                            <iframe width="560" height="315"
+                                    src="https://www.youtube.com/embed/R4_LMMKq8Hw?si=t-JhgsNTj9uHzpTz"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    referrerPolicy="strict-origin-when-cross-origin"
+                                    allowFullScreen></iframe>
+
+                            <br></br>
+
+                            <iframe width="560" height="315"
+                                    src="https://www.youtube.com/embed/RuuOAA9ekbg?si=iDNTFr0_56ADbzBs"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    referrerPolicy="strict-origin-when-cross-origin"
+                                    allowFullScreen></iframe>
+                        </li>
+                        <li>July 15: Lennon's mother, Julia Lennon, is run over and killed by a car driven by an
+                            off-duty, drunk policeman.
+                        </li>
                     </ul>
                 </p>
 
-                <p style={{ fontWeight: "bold"}}>1959</p>
+                <p style={{fontWeight: "bold"}}>1959</p>
                 <p>
                     <ul>
                         <li>September: The Quarrymen start playing regularly at the Casbah club in Liverpool, shortly after they change their name to "Johnny & the Moondogs".</li>
                     </ul>
                 </p>
+
+                <h3>1960</h3>
+                <hr></hr>
+                <br></br>
+                <p>
+                    <ul>
+                        <li>January 21: Stuart Sutcliffe buys a bass guitar in order to join the Quarrymen.</li>
+                        <li>May 5: The Quarrymen change their name to the Silver Beetles.</li>
+                        <li>August 17: The Beatles play at the Indra Club in Hamburg for the first time. They play here for the next 48 days.</li>
+                        <li>Later on in Hamburg, the boys start experimenting with Preludin, a stimulant to stay away and energetic during their extremely long sets. Sometimes playing 6-8 hours a night in the Club</li>
+                        <li>November 21: Harrison is deported from Hamburg for being under 18 and lacking a work premit. The Beatles continue without him.</li>
+                        <li>December 1: McCartney and Pete Best (their drummer at the time) are deported from Hamburg after being accused of arson.</li>
+                    </ul>
+                </p>
+
+                <h3>1961</h3>
+                <hr></hr>
+                <br></br>
+
+                <p>
+                    <li>Brian Epstein owns a record store when customers begin asking for a Beatles record called "My
+                        Bonnie." Not knowing who they are, he learns they are performing at the Cavern Club. After
+                        seeing them play and meeting with them several times, he eventually becomes their manager.
+                    </li>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/8KNedjsgnwY?si=BaovqvFnSYJzfgi2"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen></iframe>
+                    <li>Deutsche Grammophone representatives meet Epstein in Liverpool to discuss the possibility of releasing "My Bonnie" in the UK.</li>
+                </p>
+
+                <h3>1962</h3>
+                <hr></hr>
+                <br></br>
+
+                <p>
+                    <ul>
+                        <li>The Beatles record 15 songs for an audition at Decca's studio in West Hamstead.</li>
+                        <li>"My Bonnie" / "The Saints" released in UK credited to TOny Sheridan and the Beatles, the
+                            first time the group's name appear on a record
+                        </li>
+                        <li>February 6: Stuart Sutcliffe becomes sick and the boys meet Ringo Starr, who has adapted
+                            this name (orignally: Richard Starkey), because he wears some "rings" - Ringo
+                        </li>
+                        <li>Meanwhile Ringo plays in another band in Hamburg.</li>
+                        <br/>
+
+                        <p style={{fontWeight: "bold"}}>April</p>
+
+                        <li>April 10: Stuart Sutcliffe dies of a brain haemmorrhage at the age of 21.</li>
+                        <br/>
+                        <p style={{fontWeight: "bold"}}>May</p>
+
+                        <li>May 9: Epstein meets George Martin at the Abbey Road Studios and immediately sends a
+                            telegram to
+                            the Beatles in Hamburg configmung that they have been accepted.
+                        </li>
+                        <img src="https://www.brianepstein.com/images/brian-and-george-martin.jpg"
+                             alt="Brian Epstein and George Martin"
+                             width="600"/>
+
+                        <li>The Beatles return to England, they sign a recording contract with EMI's Parlophone record
+                            label, with Martin as their producer on the 4th of July. Two days later the beatles go to
+                            Abbey Road Studios for the first time, to make recording test for Parlophone. They record
+                            "Bésame Mucho", "Love Me Do", "P.S. I Love You" and "Ask Me Why".
+                        </li>
+                        <li>Martin informs Epstein that for future recordings he will use a session drummer in place of
+                            Pete Best.
+                        </li>
+                        <br/>
+
+                        <p style={{fontWeight: "bold"}}>August</p>
+
+
+                        <li>August 14: Ringo (in Hamburg) gets a phone call from Mr. Epstein to join the Beatles
+                            permanently.
+                        </li>
+                        <li>August 15-16: Best performs his last show with the Beatles, at the Cavern and a day later
+                            Lennon, McCartney and Harrison instruct Epstein to inform Best he is no longer in hte Band.
+                        </li>
+                        <li>August 18: Ringo officially joins the Beatles.</li>
+                        <li>August 23: Lennon marries Cynthia Powell in Liverpool. Their relationship was complicated;
+                            Lennon later admitted that he could be jealous and sometimes physically aggressive in his
+                            youth, something he publicly expressed regret over in later years.
+                        </li>
+                        <br/>
+                        <p style={{fontWeight: "bold"}}>September</p>
+
+                        <li>September 11: The Beatles record "P.S. I Love You" and "Love Me Do" as single. This
+                            recording is later used in the album "Please Please me".
+                        </li>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube.com/embed/0pGOFX1D_jg?si=DlOru49osrnMD4wl"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+                        <li>September - December: "Love Me Do" becomes hit</li>
+                    </ul>
+                </p>
+
+                <h3>1963</h3>
+                <hr></hr>
+                <br></br>
+
+                <p>
+                    <ul>
+                        <p style={{fontWeight: "bold"}}>February</p>
+                        <li>February 7: The Beatles record the 10 remaining tracks for their first album <a
+                            href="https://en.wikipedia.org/wiki/Please_Please_Me" target="_blank">"Please Please
+                            Me"</a> in 4 hours!!!
+                        </li>
+                        <li>(Fun-Fact): Due to recording the album within 4 hours, you can hear Lennon's voice cracking
+                            on the last track of the album: "Twist and Shout"
+                        </li>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube.com/embed/2RicaUqd9Hg?si=S0lOiipkMUrHn94O"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+                        <br/><br/>
+                        <p style={{fontWeight: "bold"}}>March</p>
+                        <li>March 22: <a href="https://en.wikipedia.org/wiki/Please_Please_Me" target="_blank">"Please
+                            Please Me"</a> is released
+                        </li>
+                        <br/>
+
+                        <p style={{fontWeight: "bold"}}>April</p>
+                        <li>April 8: <a href="https://en.wikipedia.org/wiki/Julian_Lennon" target="_blank">Julian
+                            Lennon</a> is born in Liverpool to John and Cynthia Lennon.
+                        </li>
+                        <li>April 18: The Beatles play at the BBC Light Programme concert "Swinging Sounds 63" at the
+                            Royal Alber
+                            Hall in London
+                        </li>
+                        <br/>
+
+                        <p style={{fontWeight: "bold"}}>May</p>
+                        <li>May 27: "From Me to You" / "Thank You Girl" released by Vee-Jay Records as a single</li>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube.com/embed/-XTB3bCYMK0?si=ExxvUAi31kokSOiQ"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+                        <br/><br/>
+
+                        <p style={{fontWeight: "bold"}}>August</p>
+                        <li>August 23: "She Loves You" / "I'll Get You" is released as a single in the UK.</li>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube.com/embed/S302kF8MJ-I?si=gpRixc3ytI9wHC7a"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+                        <br/><br/>
+
+                        <p style={{fontWeight: "bold"}}>October</p>
+                        <li>October 23: The Beatles fly to Sweden for a seven day tour to five cities, their first
+                            international tour as a famous group!
+                        </li>
+                        <li>October 25-29: The Boys do two 25 minute shows in Karlstad, Stockholm, Gothenburg, Borås and
+                            Eskilstuna
+                        </li>
+                        <li>October 31: The Beatles fly back to London from Sweden and are met at London Airport by
+                            10,000 fans.
+                        </li>
+                        <br/><br/>
+
+                        <p style={{fontWeight: "bold"}}>November</p>
+                        <li>November 4: The Beatles play a Royal Variety Performance at the Prince of Wales Theatre in
+                            London, in the presence of the Queen, Princess Margaret and Lord Snowdon. Where they dealed
+                            with the stage fright by rolling up a joint, and passing it to each other in the Bathroom.
+                            ;)
+                        </li>
+                        <li>Before their final number, John Lennon famously quips: <div
+                            style={{fontFamily: "cursive"}}>"The people in the cheaper seats clap
+                            your hands… and the rest of you, if you'll just rattle your jewellery."</div>
+                        </li>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube.com/embed/SYZSEWM6ZlU?si=T8ktxjmbZBTH1hdg"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+                        <br/><br/>
+
+                        <p style={{fontWeight: "bold"}}>December</p>
+                        <li>December 1: "I Want to Hold Your Hand" / "This Boy" is released</li>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube.com/embed/lW5D7HpYhBU?si=-gRQNKGDVvJgsD1a"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+                        <li>(Fun-Fact): Later in an interview George Harrison reacted to this video saying: <div style={{ fontFamily: "cursive" }}>"Good song though, not a bad song! John was as blind as a bat and he never wore his glasses, *laughing* so he couldn't see anything!"</div></li>
+                        <br/> Check out the full interview in the interviews section!
+                        <li></li>
+
+                    </ul>
+                </p>
+
 
             </article>
         </div>
