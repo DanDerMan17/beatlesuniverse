4ec79e6534c3a6e753eded559a407c59cfb96f38
fekdac22 <fekdac22@htl-kaindorf.at>
2026-05-11 10:18:08 +0200

    first article added

--- src/pages/Philosophy.tsx
+++ src/pages/Philosophy.tsx
@@ -3,14 +3,37 @@ export default function Philosophy() {
         <div className="page">
             <h2>Philosophy & Ideas</h2>
             <article>
+                <h3>How the Beatles and the Minions changed my taste in music</h3>
                 <p>
-                    Share your thoughts on the Beatles' philosophy - their messages of love,
-                    peace, and social change.
+                    My first time listening to songs from the 1960s goes back to 2015. That was the year I saw Minions in theaters for the first time. During one scene, the song You Really Got Me by The Kinks started playing in the background, and I instantly fell in love with the sound, the so called "beat music" of the era.
                 </p>
                 <p>
-                    Discuss songs like "All You Need Is Love," "Revolution," and how their
-                    music reflected the changing times of the 1960s.
+                    Around that time, I often had long discussions with my great uncle, Geza, with whom I had a very close relationship since childhood. We talked about everything imaginable. One of my biggest obsessions as a kid was dinosaurs, and I still remember memorizing entire documentaries my mother had downloaded onto DVDs for me.
                 </p>
+                <p>
+                    But after Minions, my attention shifted toward music.
+                </p>
+                <p>
+                    I told Geza about my experience hearing You Really Got Me and how I had started listening to it almost every day. He immediately recognized the song and told me he used to love it during his teenage years. He and the neighbor's son, who was around the same age, would listen to Radio Luxembourg together whenever they could. Whenever You Really Got Me came on, they would shout for each other to come outside and enjoy those two minutes of pure energy and excitement.
+                </p>
+                <p>
+                    Days, weeks, and maybe even a year later, I came across Yesterday, the first The Beatles song I intentionally listened to. At the time, I barely understood a word of English, but that didn't matter. I loved the melody, the harmonies, their appearance, and even the black-and-white video itself.
+                </p>
+                <p>
+                    After that, there was no turning back.
+                </p>
+                <p>
+                    Soon I discovered Hey Jude, and after talking to Geza again about my growing fascination with The Beatles, we began exchanging emails about music. He started sending me songs to listen to, including I'm Only Sleeping.
+                </p>
+                <p>
+                    The rest is history, and only a few clicks away from knowing nearly every lyric and every album in their discography. It was love at first listen.
+                </p>
+            </article>
+
+            <br/><br/>
+
+            <article>
+
             </article>
         </div>
     )
