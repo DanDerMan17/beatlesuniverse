db0fa050d650422e5e6c0fad3a5e3d0d3f2cf777
fekdac22 <fekdac22@htl-kaindorf.at>
2026-06-08 10:11:18 +0200

    done

--- src/pages/Philosophy.tsx
+++ src/pages/Philosophy.tsx
@@ -7,7 +7,7 @@ export default function Philosophy() {
                 <p>
                     My first time listening to songs from the 1960s goes back to 2015. That was the year I saw Minions
                     in theaters for the first time. During one scene, the song You Really Got Me by The Kinks started
-                    playing in the background, and I instantly fell in love with the sound, the so called "beat music"
+                    playing in the background, and I instantly fell in love with the sound, the so called “beat music”
                     of the era.
                 </p>
                 <p>
@@ -20,15 +20,15 @@ export default function Philosophy() {
                     But after Minions, my attention shifted toward music.
                 </p>
                 <p>
-                    I told Geza about my experience hearing You Really Got Me and how I had started listening to it
+                    I told Geza about my experience hearing _You Really Got Me_ and how I had started listening to it
                     almost every day. He immediately recognized the song and told me he used to love it during his
-                    teenage years. He and the neighbor's son, who was around the same age, would listen to Radio
-                    Luxembourg together whenever they could. Whenever You Really Got Me came on, they would shout for
+                    teenage years. He and the neighbor’s son, who was around the same age, would listen to Radio
+                    Luxembourg together whenever they could. Whenever _You Really Got Me_ came on, they would shout for
                     each other to come outside and enjoy those two minutes of pure energy and excitement.
                 </p>
                 <p>
                     Days, weeks, and maybe even a year later, I came across Yesterday, the first The Beatles song I
-                    intentionally listened to. At the time, I barely understood a word of English, but that didn't
+                    intentionally listened to. At the time, I barely understood a word of English, but that didn’t
                     matter. I loved the melody, the harmonies, their appearance, and even the black-and-white video
                     itself.
                 </p>
@@ -55,48 +55,40 @@ export default function Philosophy() {
                 <h3>The Beatles and Drugs</h3>
                 <p>
                     "That was the first time for me that I'd really smoked marijuana and uh I laughed and I laughed and
-                    I laughed it was fabulous" ~ Ringo Starr. DISCLAIMER, I DO NOT RECOMMEND ANY DRUG USE TO ANYONE,
-                    EXCEPT IF YOU ARE ABLE TO PRODUCE MUSIC LIKE THEY DID!
+                    I laughed it was fabulous" ~ Ringo Starr.
+                    **DISCLAIMER, I DO NOT RECOMMEND ANY DRUG USE TO ANYONE, EXCEPT IF YOU ARE ABLE TO PRODUCE MUSIC
+                    LIKE THEY DID!**
                 </p>
                 <p>
-                    The Beatles and in general the music in the 60s and 70s has a strong connections to drugs. But only
+                    TThe Beatles and in general the music in the 60s and 70s has a strong connections to drugs. But only
                     certain drugs. The so called "Hippie-drugs". Hippie-drugs are for instance, marijuana, LSD, and all
                     sorts of drugs, that make you calm down, and not agressive in any way. For example on the Woodstock
                     in 1969, the audience was 90% of the time high or was having a LSD-trip, however the security had to
                     be awaken all the time, so they rather took cocaine, and meth, which was strongly denied by the
                     hippies, since those kind of drugs might make you aggressive.
-                </p>
-                <p>
                     The Beatles started their drug journey during the early 60s, when they were in hamburg. Since they
                     had to play hours for hours, and being tired was not an option, so they took "Preludin", which kept
                     them awake. I mean they of course smoked cigarettes and drank coffee, but I will not count them as
                     drugs, even though they are.
-                </p>
-                <p>
                     The Beatles first encounter with pure marijuana, was in 1965, when they met Bob Dylan in America.
                     They smoked weed before that as well, but they described Dylan's herb much better than anything else
                     they had before, since they were mixing it with tobacco before. They met in a hotel room, and had a
                     great conversation. At some point Dylan rolled up a joint and passed it to Lennon, however Lennon
                     passed it to Ringo. Ringo not knowing the smoking etiquette, he finished the whole joint in the
                     corner of the room. Dylan being a bit pissed about the situation rolled up another joint and smoked
-                    it with the rest of the band.
+                    it with the rest of the band. They loved the feeling of being high. So much that it became their
+                    daily routine. Lennon later said in an interview, that during the filming of their movie "HELP",
+                    they were smoking pot for breakfast, and were often too high to memorize their scripts, or just
+                    started laughing. Lennon described smoking marijuana as: "Having a laugh"!
                 </p>
                 <p>
-                    They loved the feeling of being high. So much that it became their daily routine. Lennon later said
-                    in an interview, that during the filming of their movie "HELP", they were smoking pot for breakfast,
-                    and were often too high to memorize their scripts, or just started laughing. Lennon described
-                    smoking marijuana as: "Having a laugh"!
-                </p>
-                <p>
-                    LSD: LSD as the "wonderdrug" described by Harrison in the Dick Cavett show in the early 70s after
-                    they broke up, changed their entire sound. Shifting from song like "She Loves You!" to a much more
-                    sophisticated direction, such as: "Strawberry Fields Forever", or "Lucy in the Sky with Diamonds",
-                    which by the way spells LSD.
-                </p>
-                <p>
-                    The first ones to try the drug was, Harrison with Lennon. They shared a dentist, and the Doctor
-                    invited them and their wives at the time for dinner. Not knowing, the Doctor put LSD in their
-                    coffee, most likely to have an orgy with them, however the managed to get out of the situation.
+                    LSD as the "wonderdrug" described by Harrison in the Dick Cavett show in the early 70s after they
+                    broke up, changed their entire sound. Shifting from song like "She Loves You!" to a much more
+                    sophisticated direction, such as: "Strawberry Fields Forever", or "Lucy in the Sky with Diamonds" ->
+                    which by the way spells LSD. The first ones to try the drug was, Harrison with Lennon. They shared a
+                    dentist, and the Doctor invited them and their wives at the time for dinner. Not knowing, the Doctor
+                    put LSD in their coffee, most likely to have an orgy with them, however the managed to get out of
+                    the situation
                 </p>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/-jbTCyvKAKo?si=ci1mdTJhGMW1pcwC"
                         title="YouTube video player" frameBorder="0"
@@ -106,13 +98,11 @@ export default function Philosophy() {
                 <p>
                     Later on Lennon became addicted to heroin, through the influence by his wife Yoko, which in fact
                     isn't fun at all, however he managed to put it aside, and wrote a song called "Cold Turkey", which
-                    described the feeling of letting Heroin go.
-                </p>
-                <p>
-                    However after they broke up, none of them was much of an addict, McCartney snorted cocaine in the
-                    late 70s to 80s, however it didn't have much effect on him. Paul was often arrested though for
-                    having marijuana in his pocket. for instance in Japan, where he said in an interview, that he will
-                    never smoke weed ever again, with a smile on his face, and with a lot of sarcasm in his voice.
+                    described the feeling of letting Heroin go. However after they broke up, none of them was much of an
+                    addict, McCartney snorted cocaine in the late 70s to 80s, however it didn't have much effect on him.
+                    Paul was often arrested though for having marijuana in his pocket. for instance in Japan, where he
+                    said in an interview, that he will never smoke weed ever again, with a smile on his face, and with a
+                    lot of sarcasm in his voice.
                 </p>
             </article>
             <br/><br/>
@@ -130,9 +120,9 @@ export default function Philosophy() {
                     groups their age.
                 </p>
                 <p>
-                    Another important thing was their personality and sound. They were funny, charismatic, and different
-                    from other bands at the time. Although they came from Liverpool, their singing style sounded less
-                    heavily British, which helped them become very popular in America during the 1960s.
+                    Another important thing was their personality and sound. They were funny, charismatic, and
+                    different from other bands at the time. Although they came from Liverpool, their singing style
+                    sounded less heavily British, which helped them become very popular in America during the 1960s.
                 </p>
                 <p>
                     But the most important aspect of their music was the melody. Melody can change everything, your
@@ -157,7 +147,9 @@ export default function Philosophy() {
                         referrerPolicy="strict-origin-when-cross-origin"
                         allowFullScreen></iframe>
 
-                <br/><br/><hr/><br/>
+                <br/><br/>
+                <hr/>
+                <br/>
 
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/9_c2XZd9mMo?si=3YbZ-1L9NxH_F8jq"
                         title="YouTube video player" frameBorder="0"
@@ -170,7 +162,9 @@ export default function Philosophy() {
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         referrerPolicy="strict-origin-when-cross-origin"
                         allowFullScreen></iframe>
-                <br/><br/><hr/><br/>
+                <br/><br/>
+                <hr/>
+                <br/>
 
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/1bO9w6_iVqU?si=ujTBfpnukEY_KOPn"
                         title="YouTube video player" frameBorder="0"
